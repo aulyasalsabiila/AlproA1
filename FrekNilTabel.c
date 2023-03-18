55329fd596a30383e582e296d73b0a7e303c264b
aulyasalsabiila <128215223+aulyasalsabiila@users.noreply.github.com>
2023-03-18 15:06:30 +0700

    Add files via upload

---
+++ FrekNilTabel.c
@@ -0,0 +1,61 @@
+/* Nama Program  : FrekNilTabel */
+/* Deskripsi     : Menampilkan nilai elemen tabel T yang frekuensi kemunculannya lebih dari satu kali. */
+/* Nama Pembuat  : Aulya Salsabila Khairunnisa */
+/* NIM           : 24060122140163 */
+/* Tanggal       : 17-03-2023 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/* Kamus */
+    int *tabelPertama; /* Counter 1 */
+    int *tabelKedua; /* Counter 1 */
+    int n; /* Counter 2 */
+    int lastIndeks; /* Counter 2 */
+    int i; /* Bilangan */
+    int j; /* Bilangan */
+    int sum;
+
+/* Algoritma */
+    printf("Masukkan bilangan: ");
+    scanf("%d",&n);
+    tabelPertama = (int*)malloc(n*sizeof(int));
+    tabelKedua = (int*)malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Masukkan nilai ke-%d: ", i + 1);
+
+        scanf("%d",&tabelPertama[i]);
+
+        tabelKedua[i] = 0;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = 0;
+
+        if (tabelKedua[i] != 1)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (tabelPertama[i] == tabelPertama[j])
+                {
+                    sum++;
+                    tabelKedua[j] = 1;
+                }
+            }
+
+            if (sum > 1)
+            {
+                printf(" %d ",tabelPertama[i]);
+            }
+        }
+
+    }
+
+/* Output */
+    return 0;
+}
